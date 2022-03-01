fb12fb946e89e7bacf3451167dcb659014a22d4d
jasser2788 <mohamedjasser.benothman@esprit.tn>
2022-03-01 21:35:45 +0100

    integration

---
+++ integration_drawing_lobby/integration_drawing_lobby/server/api/getWord.js
@@ -0,0 +1,24 @@
+const getWord = () => {
+    const adjectives = [
+      "people",
+      "history",
+      "way"	,
+      "art",
+      "world"	,
+      "information",
+      "map",
+      "two",
+      "family",	
+      "government",	
+      "health"	,
+      "system"	,
+      "meat"	,
+      "year"	,
+      	
+     
+    ];
+  
+    return adjectives[Math.floor(Math.random() * adjectives.length)];
+  };
+  
+  module.exports = getWord;
